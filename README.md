be9b351752a0da64fabb9ef211373cee79e829f7
sallas-caue <sallas.bahia@gmail.com>
2023-12-11 20:00:02 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfoólio
+# Portfolio
---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    // if(html.classList.contains('light')) {
+    // } else {
+    // }
+}
